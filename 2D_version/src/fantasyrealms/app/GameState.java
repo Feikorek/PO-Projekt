912bc4e818cb293d0cc2813db104fc357d656a09
marcinpawlak <marcinpawlak0475@gmail.com>
2026-02-01 18:35:29 +0100

    Enhance GameState with level and exit location
    Added currentLevel and exitLocation fields to GameState.

--- 2D_version/src/fantasyrealms/app/GameState.java
+++ 2D_version/src/fantasyrealms/app/GameState.java
@@ -6,7 +6,6 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-// Łączy dawne GameState.java oraz pomocniczą klasę MonsterEntity
 public class GameState {
     public static final int TILE_SIZE = 48;
     public static final int MAP_SIZE = 12;
@@ -19,13 +18,14 @@ public class GameState {
     public Character player;
     public int playerX = 1;
     public int playerY = 1;
+    
+    public int currentLevel = 0; // Startuje z 0, przy pierwszej generacji wzrośnie do 1
 
-    public boolean inTown = false;
-
-    public List<Point> walls;
+    public List<Point> walls = new ArrayList<>();
     public Point townLocation = new Point(1, 1);
     public List<MonsterEntity> enemiesOnMap = new ArrayList<>();
     public Point bossLocation = new Point(10, 10);
+    public Point exitLocation = new Point(MAP_SIZE - 2, 1);
     public Monster currentEnemy = null;
 
     public GameState(Character initialPlayer, List<Point> initialWalls) {
@@ -33,7 +33,6 @@ public class GameState {
         this.walls = initialWalls;
     }
 
-    // Wewnętrzna klasa pomocnicza (dawny MonsterEntity.java)
     public static class MonsterEntity {
         public Monster monster;
         public Point location;
